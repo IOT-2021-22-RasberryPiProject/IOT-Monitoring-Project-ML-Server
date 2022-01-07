4f7a93e147f29b77ca08917a5701ed77cc3233f4
konrad-karanowski <konrad.karanowski@gmail.com>
2022-01-07 23:36:32 +0100

    First base version of classifier FPS-10

---
+++ models/abstract_classifier.py
@@ -0,0 +1,57 @@
+from typing import *
+import abc
+
+import numpy as np
+# from ..config import AttributeDict
+
+
+IMAGE_TYPE = np.ndarray 
+BOUNDING_BOX = Tuple[int, int, int, int]
+
+
+class AbstractSupportSet(abc.ABC):
+
+    _support_set: Dict[str, List[IMAGE_TYPE]]
+
+    def __init__(self, support_set: Dict[str, List[str]], config, **kwargs) -> None:
+        self._config = config
+        self._support_set: Dict[str, List[IMAGE_TYPE]] = self._get_support_set(support_set)
+        self._assert_support_set_size()
+
+    def _assert_support_set_size(self) -> None:
+        n_shot = len(self._support_set[next(iter(self._support_set))])
+        for key in self._support_set.keys():
+            candidate_n_shot = len(self._support_set[key])
+            assert n_shot == candidate_n_shot, f'Every class should contain the same number of samples but got: {candidate_n_shot} and {n_shot}'
+
+    @abc.abstractmethod
+    def _get_support_set(support_set: Dict[str, List[str]]) -> Dict[str, List[IMAGE_TYPE]]:
+        pass
+        
+    @property
+    def n_way(self) -> int:
+        return len(self._support_set.keys())
+
+    @property
+    def n_shot(self) -> int:
+        return len(self._support_set[next(iter(self._support_set))])
+
+    def __getitem__(self, idx: str) -> Iterable[IMAGE_TYPE]:
+        return self._support_set[idx]
+
+    def items(self):
+        return self._support_set.items()
+
+
+
+class AbstractClassifier(abc.ABC):
+    
+
+    def __init__(self, support_set: AbstractSupportSet, config, **kwargs):
+        self._support_set = support_set
+        self._config = config
+
+
+    @abc.abstractmethod
+    def predict(self, frame: IMAGE_TYPE) -> List[Tuple[str, float, BOUNDING_BOX]]:
+        pass
---
+++ models/opencv_fr_classifier.py
@@ -0,0 +1,112 @@
+from typing import *
+
+import time 
+import cv2
+import face_recognition
+import numpy as np
+
+from models.abstract_classifier import AbstractSupportSet, AbstractClassifier, IMAGE_TYPE, BOUNDING_BOX
+
+MODEL = "cnn" # convolutional
+
+
+
+class OpenCVSupportSet(AbstractSupportSet):
+    
+    def __init__(self, support_set: Dict[str, List[str]], config, **kwargs) -> None:
+        super(OpenCVSupportSet, self).__init__(support_set, config, **kwargs)
+
+    def _get_support_set(self, support_set: Dict[str, List[str]]) -> Dict[str, List[IMAGE_TYPE]]:
+        _set: Dict[str, List[IMAGE_TYPE]] = {}
+        for key in support_set.keys():
+            image_embeddings = []
+            for path in support_set[key]:
+                try:
+                    img = self._load_image(path)
+                    embedding = self._get_embedding(img)
+                    image_embeddings.append(embedding)
+                except IndexError as e:
+                    print(f"Couldn't recognize face at image: {path}, consider other image")
+            _set[key] = image_embeddings
+        return _set
+
+    def _load_image(self, path: str) -> np.ndarray:
+            img = cv2.imread(path)
+            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            return rgb_img
+
+    def _get_embedding(self, img: np.ndarray) -> np.ndarray:
+        embedding = face_recognition.face_encodings(img)[0]
+        return embedding
+
+
+class OpenCVClassifier(AbstractClassifier):
+    
+    def __init__(self, support_set: OpenCVSupportSet, config, **kwargs):
+        super(OpenCVClassifier, self).__init__(support_set, config, **kwargs)
+        self.detector = cv2.CascadeClassifier('/home/konrad/PythonProjects/IOT-Monitoring-Project/models/haar.xml')
+
+    def _compute_score(self, face_embeddings: List[np.ndarray], captured_face: IMAGE_TYPE) -> List[float]:
+        return face_recognition.face_distance(face_embeddings, captured_face)
+
+    def _process_scores(self, scores: List[Tuple[str, List[float]]]) -> Tuple[str, float]:
+        means = list(
+            map(lambda x: (x[0], np.mean(x[1])), scores)
+        )
+        best_result = min(means, key=lambda x: x[1])
+        return best_result
+
+    def predict(self, img: IMAGE_TYPE) -> List[Tuple[str, float, BOUNDING_BOX]]:
+        # convert BGR2RGB may be redundant in the future!
+        # if config.bgr:
+        if True:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # get location
+        print(img.shape)
+        t1 = time.time()
+        # face_locations = face_recognition.face_locations(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), model='hog')
+        rects = self.detector.detectMultiScale(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), scaleFactor=1.1, 
+            minNeighbors=5, minSize=(30, 30))
+        # OpenCV returns bounding box coordinates in (x, y, w, h) order
+        # but we need them in (top, right, bottom, left) order, so we
+        # need to do a bit of reordering
+        face_locations = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+        t2 = time.time()
+        face_encodings = face_recognition.face_encodings(img, face_locations)
+        t3 = time.time()
+
+        # result
+        result: List[Tuple[str, float, BOUNDING_BOX]] = []
+        for bounding_box, face_encoding in zip(face_locations, face_encodings):
+            all_scores = []
+            for key, item in self._support_set.items():
+                scores = self._compute_score(item, face_encoding)
+                all_scores.append((key, scores))
+            t4 = time.time()
+            best_result = self._process_scores(all_scores)
+            result.append(
+                (best_result[0], 1 - best_result[1], bounding_box)
+            )
+            print(f'-> {t2 - t1}, 2 ->{t3 -t2}, 3 -> {t4-t3}')
+        return result
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    support_set = OpenCVSupportSet({
+        'jan': [
+            '/home/konrad/PythonProjects/IOT-Monitoring-Project/support_set/jan_wielgus/1.jpg',
+            '/home/konrad/PythonProjects/IOT-Monitoring-Project/support_set/jan_wielgus/2.jpg'
+            ],
+        'weronika': [
+            '/home/konrad/PythonProjects/IOT-Monitoring-Project/support_set/weronika_welyczko/1.jpg',
+            '/home/konrad/PythonProjects/IOT-Monitoring-Project/support_set/weronika_welyczko/2.jpg'
+        ]
+    }, None)
+
+    classifier = OpenCVClassifier(support_set, None)
+    print(classifier.predict(cv2.imread('/home/konrad/PythonProjects/IOT-Monitoring-Project/support_set/jan_wielgus/1.jpg')))
